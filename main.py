c1e2344d55109a7c4d7222b0402ae5efb7912c59
Anton Sokolkin <97021906+ASreiros@users.noreply.github.com>
2023-02-26 15:55:55 +0200

    Player guess fixed. Computer guess done

--- main.py
+++ main.py
@@ -2,25 +2,48 @@ import art
 import random
 
 
-def guess(nr1, nr2):
-    return random.randint(nr1 + 1, nr2)
-
-
-def print_terminate_warning():
-    print(f"It looks like chosen number is higher than {low}, but lower that {high}.")
-    print("Clearly something wrong. Game will be terminated.")
-
-
-def check_number(nr, chosen_number):
-    if nr == chosen_number:
+def check_number(nr, local_chosen_number, local_low, local_high):
+    if nr == local_chosen_number:
         print("You guessed right.")
-        return True
-    elif nr > chosen_number:
+        return [True, local_low, local_high]
+    elif nr > local_chosen_number:
         print(f"Guessed number {nr} was too high. Chosen number is lower")
-        return False
+        local_high = nr
+        return [False, local_low, local_high]
     else:
+        local_low = nr
         print(f"Guessed number {nr} was too low. Chosen number is higher")
-        return False
+        return [False, local_low, local_high]
+
+
+def guess_number(local_low, local_high):
+    roll = random.randint(1, 6)
+    difference = local_high - local_low
+    if roll > 4:
+        try_number = random.randint(local_low+1, local_high-1)
+    elif difference % 2 == 1:
+        top = local_low+1 + (difference-1)/2
+        try_number = random.randint(local_low+1, top)
+    else:
+        top = local_low + 1 + difference / 2
+        try_number = random.randint(local_low+1, top)
+
+    result_flag = False
+    guess_right = False
+    while not result_flag:
+        result = input(f"Computer guessed number is {try_number}. Please type 'c' for correct number, 'h' if number is too high or 'l' if number is too low:    ")
+        if result == "c":
+            result_flag = True
+            guess_right = True
+        elif result == "h" and try_number > local_low + 1:
+            result_flag = True
+            local_high = try_number
+        elif result == "l" and try_number < local_high - 1:
+            result_flag = True
+            local_low = try_number
+        else:
+            print("That is not a valid option")
+    return [guess_right, local_low, local_high]
 
 
 play = True
@@ -46,29 +69,41 @@ while play:
         lives = 10
 
     if mode == "p":
-        chosen_number = random.randint(1, 101)
+        chosen_number = random.randint(1, 100)
         print(f"debug mode: chosen number is {chosen_number}")
+    else:
+        input(f"Think of the number  which the computer will try to guess and press any key")
 
     while mode == "p" and 100 > lives > 0:
         print(f"You have {lives} tries to guess the number")
-        player_guess = input(f"Guess a number between {low+1} and {high-1}:       ")
+        player_guess = input(f"Guess a number from {low+1} to {high-1}:       ")
         if player_guess.isdigit():
             if high > int(player_guess) > low:
-                if check_number(int(player_guess), chosen_number):
+                [reply, low, high] = check_number(int(player_guess), chosen_number, low, high)
+                if reply:
                     lives = 100
                 else:
                     lives -= 1
             else:
-                print(f"Your number have to be between {low+1} and {high-1}. Try again")
+                print(f"Your number have to be from {low} to {high}. Try again")
         else:
             print("That is not a number. At least not a valid one. Try again")
 
-
+    while mode == "c" and 100 > lives > 0:
+        print(f"Computer has {lives} tries to guess the number")
+        print(f"Computer will try to guess a number from {low+1} to {high-1}")
+        [reply, low, high] = guess_number(low, high)
+        if reply:
+            lives = 100
+        else:
+            lives -= 1
 
     # End of the game block
-    if lives == 100:
+    if lives == 100 and mode == "p" or lives == 0 and mode == "c":
         print("You win")
+    elif lives == 100 and mode == "c":
+        print("Computer guessed your number. Game over")
     else:
-        print("You lose. Game over")
+        print(f"You lose. Game over. Chosen number was {chosen_number}")
     if input("Do you want to play again? Type 'y' for yes or 'n' for no:    ").lower() != "y":
         play = False
